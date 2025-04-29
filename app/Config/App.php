6e9b1cc465010db76771d3367238f992b017e833
frannSDR <frannm1703@gmail.com>
2025-04-28 22:11:28 -0300

    project name changed

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/proyecto_fran';
+    public string $baseURL = 'http://localhost/proyecto_kw_store';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
